29c67708bdd432f395929244af832e5f623d45aa
LunarLogician <151429872+LunarLogician@users.noreply.github.com>
2024-07-06 22:05:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", iclick);
+
+    function iclick() {
+    var buttonin = this.innerHTML;
+       
+       makesound(buttonin);
+    }
+
+}
+
+document.addEventListener("keypress",function(event){
+    makesound(event.key , this);
+makesound(event.key);
+});
+
+function makesound(key){
+
+    switch (key) {
+        case "w":
+           var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            this.style.color="white"
+
+            break;w
+        case "a":
+
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+
+        default:
+            alert("not allowed");
+            break;
+    }
+}
